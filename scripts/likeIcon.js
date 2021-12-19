5ebc2dfdedb1cea02bb08954c4222d6b0fd1a67e
eugenvetrov <evg.vetrow@yandex.ru>
2021-12-19 05:12:25 +0500

    Сделать часть с лайками

---
+++ scripts/likeIcon.js
@@ -0,0 +1,15 @@
+const likeIcon = document.querySelectorAll(".group__like-icon");
+Array.from(likeIcon).forEach((element, i) => {
+    console.log(likeIcon[i].src)
+    likeIcon[i].addEventListener("click", (event) => {
+        if (likeIcon[i].getAttribute("src") == "./images/like.svg"){
+            likeIcon[i].setAttribute("src", "./images/like-active.svg");
+            likeIcon[i].classList.add("group__like-icon_active");
+        } else if (likeIcon[i].getAttribute("src") == "./images/like-active.svg") {
+            likeIcon[i].setAttribute("src", "./images/like.svg");
+            likeIcon[i].classList.remove("group__like-icon_active");
+        }
+    }
+    );
+    }
+);
